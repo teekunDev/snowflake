bdea82353016f08ae91762da18229ef4ca3d659a
keifufu <github@keifufu.dev>
2023-09-01 14:38:21 +0200

    update comments

--- files/scripts/screenshot.sh
+++ files/scripts/screenshot.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# This uploads to https://github.com/keifufu/yass
+
 TOKEN=$(cat $NIXOS_SECRETS/screenshot_token);
 HOST=$(cat $NIXOS_SECRETS/screenshot_host);
 FILENAME=$(date '+%y-%m-%dT%H-%M-%S.png')
