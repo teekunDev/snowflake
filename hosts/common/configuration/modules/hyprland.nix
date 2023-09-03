38a769407161dd2c5f7b0c01737724d26297507e
keifufu <github@keifufu.dev>
2023-09-03 06:39:30 +0200

    enable hyprland cachix

--- hosts/common/configuration/modules/hyprland.nix
+++ hosts/common/configuration/modules/hyprland.nix
@@ -60,8 +60,8 @@
     AllowHybridSleep=yes
   '';
 
-#  nix.settings = {
-#    substituters = ["https://hyprland.cachix.org"];
-#    trusted-public-keys = ["hyprland.cachis.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
-#  };
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachis.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
 }
