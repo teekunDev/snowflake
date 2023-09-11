373183b68a46c10b3d687b1183a3146d7d03a06a
keifufu <github@keifufu.dev>
2023-09-11 16:37:54 +0200

    todo

--- files/scripts/screenshot.sh
+++ files/scripts/screenshot.sh
@@ -5,9 +5,9 @@ HOST=$(cat $NIXOS_SECRETS/yass_host);
 FILENAME=$(date '+%y-%m-%dT%H-%M-%S.png')
 
 # TODO: freeze screen
+# TODO: better feedback (notifications or smth)
 
 region="$(slurp)"
 if [[ -n "$region" ]]; then
   grim -g "$region" - | convert - -shave 1x1 PNG:- | swappy -f - -o - | curl -X PUT $HOST/upload?filename=$FILENAME -H "Authorization: $TOKEN" -H "Content-Type: application/octet-stream" --data-binary "@-" | wl-copy
-  notify-send -t 3000 "Uploaded screenshot"
 fi
