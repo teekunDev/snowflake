bd139b41bea5c1ac80a582000f76166a764687bc
keifufu <github@keifufu.dev>
2023-09-12 09:22:33 +0200

    update locale

--- hosts/common/configuration/configuration.nix
+++ hosts/common/configuration/configuration.nix
@@ -61,10 +61,6 @@
   time.timeZone = "Europe/Berlin";
   i18n = {
     defaultLocale = "en_US.UTF-8";
-    extraLocaleSettings = {
-      LC_TIME = "de_DE.UTF-8";
-      LC_MONETARY = "de_DE.UTF-8";
-    };
   };
 
   console = {
