d2f3ef21fabb80b2a615fd77c3db4508874bbfbf
keifufu <github@keifufu.dev>
2023-09-25 15:09:09 +0200

    update mullvad config

--- hosts/common/configuration/modules/mullvad.nix
+++ hosts/common/configuration/modules/mullvad.nix
@@ -18,9 +18,12 @@
     mullvad = config.services.mullvad-vpn.package;
   in ''
     while ! ${mullvad}/bin/mullvad status >/dev/null; do sleep 1; done
-    ${mullvad}/bin/mullvad auto-connect set on
+    ${mullvad}/bin/mullvad auto-connect set off
     ${mullvad}/bin/mullvad lan set allow
     ${mullvad}/bin/mullvad dns set custom 1.1.1.1 1.0.0.1
     ${mullvad}/bin/mullvad relay set location de fra
+    ${mullvad}/bin/mullvad relay set tunnel wireguard --entry-location any
+    ${mullvad}/bin/mullvad relay set tunnel-protocol wireguard
+    ${mullvad}/bin/mullvad relay set ownership owned
   '';
 }
