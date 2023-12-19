17ff063c1bb9b15192916f47a4059fc14fedc5cf
keifufu <github@keifufu.dev>
2023-12-19 11:56:41 +0100

    latest osu-lazer

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -8,8 +8,22 @@
 #           └─ home.nix *
 #
 
-{ pkgs, ... }:
+# https://github.com/NixOS/nixos-search/issues/636#issuecomment-1477885841
 
+let
+  newestPkgs = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/master.tar.gz";
+  }) { config = { allowUnfree = true; }; };
+
+  overlay = final: prev: {
+    osu-lazer-bin = newestPkgs.osu-lazer-bin;
+  };
+
+  pkgs = import <nixpkgs> {
+    overlays = [ overlay ];
+    config = { allowUnfree = true; };
+  };
+in
 {
   home.packages = with pkgs; [
     osu-lazer-bin
