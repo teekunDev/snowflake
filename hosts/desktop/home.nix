e0877b7efc10a27ddc5430c44b2883cb377800fa
keifufu <github@keifufu.dev>
2023-12-19 14:41:09 +0100

    new osu 2

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -8,24 +8,13 @@
 #           └─ home.nix *
 #
 
-# https://github.com/NixOS/nixos-search/issues/636#issuecomment-1477885841
+{ pkgs, ... }:
 
 let
-  newestPkgs = import (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/master.tar.gz";
-  }) { config = { allowUnfree = true; }; };
-
-  overlay = final: prev: {
-    osu-lazer-bin = newestPkgs.osu-lazer-bin;
-  };
-
-  pkgs = import <nixpkgs> {
-    overlays = [ overlay ];
-    config = { allowUnfree = true; };
-  };
+  osu = pkgs.callPackage ../../pkgs/osu-lazer/bin.nix { };
 in
 {
-  home.packages = with pkgs; [
-    osu-lazer-bin
+  home.packages = [
+    osu
   ];
 }
